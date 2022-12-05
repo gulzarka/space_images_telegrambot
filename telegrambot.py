bebd86fc3ae1798f35a7f34270e6ad70c4b332b6
gulzarka <80506357+gulzarka@users.noreply.github.com>
2022-12-05 17:54:53 +0600

    publish text to Telegram channel.

--- telegrambot.py
+++ telegrambot.py
@@ -3,5 +3,4 @@ import telegram
 bot = telegram.Bot(token='5843213600:AAG-BjATLJg5n_stXWYQelOUCal8rS0CbPs')
 updates = bot.get_updates()
 chat_id = bot.get_updates()[-1].message.chat_id
-print(chat_id)
-bot.send_message(chat_id=chat_id, text="Hello everyone!")
+bot.send_message(chat_id='@myspaceimages', text="Hello everyone!")
