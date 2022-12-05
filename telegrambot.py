355c7526dc6526211839a8ae8ce4fd388755bd38
gulzarka <80506357+gulzarka@users.noreply.github.com>
2022-12-05 17:34:14 +0600

    First message

--- telegrambot.py
+++ telegrambot.py
@@ -1,4 +1,7 @@
 import telegram
 
 bot = telegram.Bot(token='5843213600:AAG-BjATLJg5n_stXWYQelOUCal8rS0CbPs')
-print(bot.get_me())
+updates = bot.get_updates()
+chat_id = bot.get_updates()[-1].message.chat_id
+print(chat_id)
+bot.send_message(chat_id=chat_id, text="Hello everyone!")
