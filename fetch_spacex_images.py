920ee6803b6463ac01bb9e29934f833a1d603f78
gulzarka <80506357+gulzarka@users.noreply.github.com>
2022-12-20 23:46:48 +0600

    - deleted commented code

--- fetch_spacex_images.py
+++ fetch_spacex_images.py
@@ -13,39 +13,6 @@ def fetch_spacex_images(launch_id):
         path = 'images'
         filename = f'{number}spacex.jpg'
         download_images(image_link, path, filename)
-   
-
-     
-def fetch_spacex_last_launch(launch_id):
-    response = requests.get("https://api.spacexdata.com/v5/launches/{0}".format(launch_id))
-    response.raise_for_status()
-    response_content = response.json()
-    image_links = response_content['links']['flickr']['original']
-    if not image_links == []:
-        for number, image_link in image_links:
-            path = 'images'
-            filename = f'{number}spacex.jpg'
-            download_images(image_link, path, filename)
-    else:
-        print('No launch images uploaded yet...')
-
-
-# def get_launch_id_list():
-#     response = requests.get("https://api.spacexdata.com/v5/launches")
-#     response.raise_for_status()
-#     response_content = response.json()
-#     launch_id_list = []
-#     for content in response_content:
-#         if not content['links']['flickr']['original'] == []:
-#             launch_id = content['id']
-#             launch_id_list.append(launch_id)
-#     print(launch_id_list)   
-
-
-# def get_random_launch_id():
-#     launch_id_list = get_launch_id_list()
-#     random_launch_id = random.choice(launch_id_list)
-#     return random_launch_id
 
 
 def create_arguments():
