067a24fbfb559c526dd90c4d30d29370826b29dc
gulzarka <80506357+gulzarka@users.noreply.github.com>
2022-12-22 16:57:50 +0600

    fixed bug in params variable

--- download_images.py
+++ download_images.py
@@ -4,8 +4,8 @@ import requests
 from urllib.parse import urlsplit, unquote
 
 
-def download_images(url, path, filename, params):
-    response = requests.get(url, params=None)
+def download_images(url, path, filename, params=None):
+    response = requests.get(url, params=params)
     response.raise_for_status()
     os.makedirs(path, exist_ok=True)
     with open(os.path.join(path, filename),'wb') as file:
