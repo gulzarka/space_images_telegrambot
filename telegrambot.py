71a75599c62d44b235f27e8f9243ac4488bc5b06
gulzarka <80506357+gulzarka@users.noreply.github.com>
2022-12-28 01:04:55 +0600

    updates

--- telegrambot.py
+++ telegrambot.py
@@ -2,7 +2,6 @@ import os
 import time
 import random
 import argparse
-
 import telegram
 from dotenv import load_dotenv
 
@@ -11,20 +10,27 @@ def send_image(bot, channel, directory, file):
          bot.send_photo(chat_id=channel, photo=image)
 
 
-def post_selected_image(bot, channel, file, directory='images'):
-    send_image(bot, channel, directory, file)
-    
+def post_selected_image(bot, channel, file):
+    directory = 'images'
+    try:
+        send_image(bot, channel, directory, file)
+    except telegram.error.TelegramError:
+            print('connection error occured')   
 
-def post_shuffled_images(bot, channel, directory='images', timer=14400):
+
+def post_shuffled_images(bot, channel, directory, timer=14400):
     while True:
-        files = list(os.walk(directory))[0][2]
-        random.shuffle(files)
-        for file in files:
-            send_image(bot,channel,directory,file)
-            time.sleep(timer)
-       
-
-         
+            files = list(os.walk(directory))[0][2]
+            random.shuffle(files)
+            for file in files:
+                try:
+                    send_image(bot,channel,directory,file)
+                    time.sleep(int(timer))
+                    time.sleep(3)
+                except telegram.error.TelegramError:
+                    print('connection error occured')       
+            
+        
 def create_arguments():
     parser = argparse.ArgumentParser(description='Post images to telegram channel')
     parser.add_argument(
@@ -61,16 +67,10 @@ def main():
     folder = args.f
     image = args.i
     if image:
-        try:
-            post_selected_image(bot, channel, image, folder)
-        except telegram.error.TelegramError:
-            print('Telegram network error has occured')
+        post_selected_image(bot, channel, image)
     else:
-        try:
-            post_shuffled_images(bot, channel, folder, timer)
-        except telegram.error.TelegramError:
-            print('Telegram network error has occured')
-
+        post_shuffled_images(bot, channel, folder, timer)     
+               
 
 if __name__ == '__main__':
     main()
